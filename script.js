902ef1d2542c2780c4f2e3ed32d87757cbac7bb4
Leandro Junior <juninhoromagnoli11@gmail.com>
2024-07-23 14:04:30 -0400

    updates
    changed almost everything.
    I was struggling with my approach, so I researched some completed challenges in the Frontend Mentor discord and now Im trying a different way to write this project.

---
+++ script.js
@@ -0,0 +1,29 @@
+let labels = document.querySelectorAll("label");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", showText);
+});
+
+function showText() {
+  button1.addEventListener("click", (event) => {
+    if (textButton1.style.display === "none") {
+      textButton1.display = "block";
+      // } else if (labels.style.display === "block") {
+      //   labels.style.display = "none";
+      //   textButton1.display = "block";
+    }
+    // else if (textButton1.style.display === "block") {
+    //   textButton1.style.display = "none";
+    // }
+  });
+
+  console.log("you pressed");
+}
+
+//  labels.forEach((label) => {
+//   if (label.style.display === "none") {
+//     label.style.display = "block";
+//   } else {
+//     label.style.display = "none";
+//   }
+// });
