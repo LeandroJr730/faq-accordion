dd54ad5f14534f7bd6ed4da18e133728200274dc
Leandro Junior <juninhoromagnoli11@gmail.com>
2024-07-23 10:46:00 -0400

    changed the approach in js
    I was trying to toggle between classes in js, but now im trying to use the built in style changer

--- index.js
+++ index.js
@@ -1,53 +1,21 @@
-let buttons = document.querySelectorAll(".buttons");
-let labels = document.querySelectorAll("[name='labels']");
-const hiddenTexts = document.querySelectorAll(".hide-text");
+const buttons = document.querySelectorAll(".buttons");
+const button1 = document.getElementById("button-1");
+const textButton1 = document.getElementById("text-button-1");
+
+let labels = document.querySelectorAll("label");
 
 buttons.forEach((button) => {
   button.addEventListener("click", showText);
 });
 
 function showText() {
-  if (hiddenTexts.classList.contains("hide-text")) {
-    hideText.classList.replace("show-text", "hide-text");
-  } else {
-    hideText.classList.replace("hide-text", "show-text");
-  }
+  labels.forEach((label) => {
+    if (label.style.display === "none") {
+      label.style.display = "block";
+    } else {
+      label.style.display = "none";
+    }
+  });
+
   console.log("you pressed");
 }
-
-// function showText() {
-//   if (labels.classList.contains("hide-text")) {
-//     hideText.classList.replace("hide-text", "show-text");
-//   } else {
-//     hideText.classList.replace("show-text", "hide-text");
-//   }
-//   console.log("you pressed");
-// }
-
-// let buttons = document.querySelectorAll(".myButtons");
-
-// buttons.forEach((button) => {
-//   button.classList.add("enabled");
-// });
-
-// buttons.forEach((button) => {
-//   button.addEventListener("mouseover", (event) => {
-//     event.target.classList.toggle("hover");
-//   });
-// });
-
-// buttons.forEach((button) => {
-//   button.addEventListener("mouseout", (event) => {
-//     event.target.classList.toggle("hover");
-//   });
-// });
-
-// buttons.forEach((button) => {
-//   button.addEventListener("click", (event) => {
-//     if (event.target.classList.contains("disabled")) {
-//       event.target.textContent += "😒😡";
-//     } else {
-//       event.target.classList.replace("enabled", "disabled");
-//     }
-//   });
-// });
