8a8f53d21f53c6b4b5b3fbddfd60d3a9f4eeec34
Leandro Junior <juninhoromagnoli11@gmail.com>
2024-07-26 10:24:05 -0400

    finished
    finished everything.
    I gave up on this project, I could not do the JS code, I copied from https://www.frontendmentor.io/solutions/faq-accordion-using-javascript-L6GJ5f3-lt   .
    I ended up just adjusting my project to the code that I copied.
    Im proud of doing the "mobile and desktop conversions" on my own.

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-const details = document.querySelectorAll(".details");
+const blocks = document.querySelectorAll(".block");
+const answers = document.querySelectorAll(".answers");
+blocks.forEach((block) => {
+  const question = block.querySelector(".question");
+  const icon = block.querySelector(".question .expand-icon");
+  const currentAnswer = block.querySelector(".answer");
 
-buttons.forEach((button) => {
-  button.addEventListener("click", showText);
-});
-
-function showText() {
-  button1.addEventListener("click", (event) => {
-    if (textButton1.style.display === "none") {
-      textButton1.display = "block";
-      // } else if (labels.style.display === "block") {
-      //   labels.style.display = "none";
-      //   textButton1.display = "block";
+  question.addEventListener("click", () => {
+    currentAnswer.classList.toggle("active");
+    if (currentAnswer.classList.contains("active")) {
+      icon.src = "./assets/images/icon-minus.svg";
+      currentAnswer.style.maxHeight = currentAnswer.scrollHeight + "px";
+    } else {
+      icon.src = "./assets/images/icon-plus.svg";
+      currentAnswer.style.maxHeight = 0;
     }
-    // else if (textButton1.style.display === "block") {
-    //   textButton1.style.display = "none";
-    // }
+    blocks.forEach((b) => {
+      const ans = b.querySelector(".answer");
+      const icon = b.querySelector(".expand-icon");
+      if (ans !== currentAnswer) {
+        ans.classList.remove("active");
+        ans.style.maxHeight = 0;
+        icon.src = "./assets/images/icon-plus.svg";
+      }
+    });
   });
-
-  console.log("you pressed");
-}
-
-//  labels.forEach((label) => {
-//   if (label.style.display === "none") {
-//     label.style.display = "block";
-//   } else {
-//     label.style.display = "none";
-//   }
-// });
+});
